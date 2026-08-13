4559ef91383ed73091c4a064829142b80a4489b4
채영준 <157681635+cyjoon68@users.noreply.github.com>
2026-08-13 17:03:26 +0900

    chore(app): promote Cloud Wave 1 BOM (#2)

--- dev/localstack/10-shopport.sh
+++ dev/localstack/10-shopport.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -eu
 
-awslocal s3api create-bucket \
-  --bucket shopport-assets \
-  --create-bucket-configuration LocationConstraint=ap-northeast-2
+create_bucket() {
+  bucket="$1"
+  if ! awslocal s3api head-bucket --bucket "$bucket" >/dev/null 2>&1; then
+    awslocal s3api create-bucket \
+      --bucket "$bucket" \
+      --create-bucket-configuration LocationConstraint=ap-northeast-2
+  fi
+}
+
+create_bucket shopport-assets-raw
+create_bucket shopport-assets-normalized
+create_bucket shopport-archives
 awslocal sqs create-queue --queue-name shopport-asset-results
 awslocal sqs create-queue --queue-name shopport-asset-results-dlq
